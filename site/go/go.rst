b2798a28b3489e86841122f72fd360a261cd9b1f
halfming <halfming@gmail.com>
2013-07-01 11:19:57 -0400

    test

---
+++ site/go/go.rst
@@ -0,0 +1,6 @@
+安装
+
+  1. export GOROOT=~/go
+  2. hg clone -r release https://go.googlecode.com/hg/ $GOROOT
+  3. export PATH=$GOROOT/bin:$PATH
+
---
+++ site/python/LPTHW/ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74
+my_weight = 180
+my_eye = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "let's talk about %s." % my_name
+print "let's talk about %s." % my_height
+print "let's talk about %s." % my_weight
+print "let's talk about %s." % my_name
+print "let's talk about %s and %s ." % (my_name, my_hair)
+print "If I add %d, %d, and %d I get %d" % (
+        my_age, my_weight, my_age,my_age + my_height + my_weight)
